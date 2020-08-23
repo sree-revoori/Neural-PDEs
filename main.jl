08ed509e14bb238ff727ec9b27adae56315617ab
Sree Revoori <srrevoori@gmail.com>
2020-08-23 16:48:12 -0400

    Update main.jl

--- main.jl
+++ main.jl
@@ -9,4 +9,8 @@ Nx = size(x)
 
 u0 = exp.(-(x.-3.0).^2) # I.C
 
+## Problem Parameters
+p        = [1.0,1.0]    # True solution parameters
+xtrs     = [dx,Nx]      # Extra parameters
+dt       = 0.40*dx^2    # CFL condition
 
