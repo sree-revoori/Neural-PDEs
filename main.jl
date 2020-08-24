e2624bc2c315d6fb3c41dbdca3ed76a5e4ee1ff1
Sree Revoori <srrevoori@gmail.com>
2020-08-24 09:52:44 -0400

    Update main.jl

--- main.jl
+++ main.jl
@@ -13,4 +13,7 @@ u0 = exp.(-(x.-3.0).^2) # I.C
 p        = [1.0,1.0]    # True solution parameters
 xtrs     = [dx,Nx]      # Extra parameters
 dt       = 0.40*dx^2    # CFL condition
+t0, tMax = 0.0 ,1000*dt
+tspan    = (t0,tMax)
+t        = t0:dt:tMax;
 
