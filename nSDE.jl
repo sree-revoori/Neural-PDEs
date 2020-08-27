4b5f96a185c85d77c17c82d43504a2af77306b27
Sree Revoori <srrevoori@gmail.com>
2020-08-27 09:02:03 -0400

    Update nSDE.jl

--- nSDE.jl
+++ nSDE.jl
@@ -5,3 +5,8 @@ u0 = Float32[2.; 0.]
 datasize = 30
 tspan = (0.0f0, 1.0f0)
 tsteps = range(tspan[1], tspan[2], length = datasize)
+
+function trueSDEfunc(du, u, p, t)
+    true_A = [-0.1 2.0; -2.0 -0.1]
+    du .= ((u.^3)'true_A)'
+end
