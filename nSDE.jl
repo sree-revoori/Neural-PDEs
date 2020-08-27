4c95c88a137f8e4b44aec52ce4ed970e055c5320
Sree Revoori <srrevoori@gmail.com>
2020-08-27 09:01:50 -0400

    Update nSDE.jl

--- nSDE.jl
+++ nSDE.jl
@@ -1,2 +1,7 @@
 using Plots, Statistics
 using Flux, DiffEqFlux, StochasticDiffEq, DiffEqBase.EnsembleAnalysis
+
+u0 = Float32[2.; 0.]
+datasize = 30
+tspan = (0.0f0, 1.0f0)
+tsteps = range(tspan[1], tspan[2], length = datasize)
